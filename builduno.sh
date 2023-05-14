04f175b7b80fa7778e2ee23232abdc21705b1b28
rock5b_lhn <pixelhn@qq.com>
2023-05-14 12:47:28 +0800

    整理文件

--- builduno.sh
+++ builduno.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-./arduino-cli compile --fqbn arduino:avr:uno $1 || exit
+arduino-cli compile --fqbn arduino:avr:uno $1 || exit
 
-./arduino-cli upload -p /dev/ttyACM1 --fqbn arduino:avr:uno  $1
+arduino-cli upload -p /dev/ttyACM1 --fqbn arduino:avr:uno  $1
+
+arduino-cli monitor -p /dev/ttyACM1  --config 115200
